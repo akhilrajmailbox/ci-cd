01529dbfed33ee01606c844de9b6447ab543bb3e
Akhil Raj <akhil@vivasa.in>
2017-06-14 12:17:39 +0530

    Role based strategy

---
+++ Role-Strategy-Plugin-Jenkins_files/navigation.js
@@ -0,0 +1,8 @@
+/* Jquery Toggle Function for Menu Navigation */
+/* Responsive ? */
+
+jQuery( document ).ready( function( $ ) {
+jQuery( ".menu-toggle" ).click( function () {
+	jQuery( ".menu" ).toggle();
+});
+});
